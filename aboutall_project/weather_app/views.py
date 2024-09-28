3edc99f213aca48abc279578823f7a2823d08cd6
weidermartenn <weidermartenn@gmail.com>
2024-09-28 21:58:04 +0300

    иконки

--- aboutall_project/weather_app/views.py
+++ aboutall_project/weather_app/views.py
@@ -3,27 +3,18 @@ import requests
 import requests.exceptions
 from .models import City
 
+weather_icons = {
+    'Clear' : '/assets/icon-sun.svg',
+    'Clouds' : './assets/icon-clouds.svg',
+    'Rain' : './assets/icon-rain.svg',
+}
+
+# def weather(request):
+#     API_KEY = '02586546b81e214ba2c5b751c986d399'
+#     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}"
+
+    
+#     return render(request, './weather.html')
+
 def weather(request):
-    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=02586546b81e214ba2c5b751c986d399"
-
-    cities = City.objects.all()
-
-    weather_data = []
-
-    for city in cities:
-        city_name = city.name
-        try:
-            city_weather = requests.get(url.format(city_name), timeout=5).json()
-        except requests.exceptions.ReadTimeout:
-            print('timeout')
-            continue
-        
-        weather = {
-            'city': city_name,
-            'temperature': city_weather['main']['temp'],
-        }
-
-        weather_data.append(weather)
-
-    context = {'weather_data' : weather_data}
-    return render(request, './weather.html', context)
+    return render(request, './weather.html')
